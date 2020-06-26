c45ad8e46f0a7b8b66e6335250b00f2225b179e0
Guido Lange <g.lange@thalia.de>
2020-06-26 17:52:19 +0200

    Add header to App

--- src/public/react/src/App.tsx
+++ src/public/react/src/App.tsx
@@ -2,25 +2,30 @@ import React from 'react';
 import './App.css';
 import routes from './util/routes';
 import { Route, Switch } from 'react-router-dom';
-import HomePage from './pages/Home/HomePage';
+import HomePage from './pages/HomePage';
 import SkipboOverviewPage from './pages/SkipboOverviewPage';
 import SkipboAddGamePage from './pages/SkipboAddGamePage';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header" />
-      <Switch>
-        <Route path={routes.skipboAddGame}>
-          <SkipboAddGamePage />
-        </Route>
-        <Route path={routes.skipbo}>
-          <SkipboOverviewPage />
-        </Route>
-        <Route path={routes.home}>
-          <HomePage />
-        </Route>
-      </Switch>
+      <header className="App-header">
+        <Header />
+      </header>
+      <main className="App-main">
+        <Switch>
+          <Route path={routes.skipboAddGame}>
+            <SkipboAddGamePage />
+          </Route>
+          <Route path={routes.skipbo}>
+            <SkipboOverviewPage />
+          </Route>
+          <Route path={routes.home}>
+            <HomePage />
+          </Route>
+        </Switch>
+      </main>
     </div>
   );
 }
