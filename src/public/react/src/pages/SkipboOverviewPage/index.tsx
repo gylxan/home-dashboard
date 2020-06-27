d8516b949dce1d3b15be4b170388ec9a1e92c25d
Guido Lange <g.lange@thalia.de>
2020-06-27 12:38:36 +0200

    Set page title and use new GeneralStatisticCard in SkipboOverviewPage

--- src/public/react/src/pages/SkipboOverviewPage/index.tsx
+++ src/public/react/src/pages/SkipboOverviewPage/index.tsx
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from 'react';
-import { getSkipboGames } from '../../util/apiclient';
-import { SkipboGame } from '../../interfaces/skipboGame';
 import { Button } from 'react-bootstrap';
-import { linkTo } from '../../util/routes';
 import { Link } from 'react-router-dom';
-
+import { getSkipboGames, getSkipboGameStatisticsGeneral } from '../../util/apiclient';
+import { SkipboGame } from '../../interfaces/skipboGame';
+import { getPageTitle, linkTo } from '../../util/routes';
+import GeneralStatisticCard from '../../components/GeneralStatisticCard';
 
 import styles from './SkipboOverviewPage.module.css';
 
@@ -12,11 +12,13 @@ function SkipboOverviewPage() {
   const [skipboGames, setSkipboGames] = useState([] as SkipboGame[]);
 
   useEffect(() => {
+    document.title = getPageTitle('Skip-Bo');
     getSkipboGames().then((data) => setSkipboGames(data));
   }, []);
 
   return (
     <div className="SkipboOverviewPage">
+      <GeneralStatisticCard title={'Allgemein'} fetchData={getSkipboGameStatisticsGeneral} />
       <ul>
         {skipboGames.map((game) => (
           <li>{`${new Date(game.playTime).toLocaleString('de-DE')} - Gewinner: ${game.winner.name}`}</li>
