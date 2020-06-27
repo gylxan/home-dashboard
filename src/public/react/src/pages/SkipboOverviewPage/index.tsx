89ee4bdb2a51c1384b25aa2a766555c1a559e319
Guido Lange <g.lange@thalia.de>
2020-06-27 16:35:21 +0200

    Removed unused stuff

--- src/public/react/src/pages/SkipboOverviewPage/index.tsx
+++ src/public/react/src/pages/SkipboOverviewPage/index.tsx
@@ -1,11 +1,10 @@
 import React, { useEffect, useState } from 'react';
-import { Button, CardColumns, Spinner, Alert } from 'react-bootstrap';
+import { Alert, Button, CardColumns, Spinner } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import {
   getSkipboGames,
   getSkipboGamesPerWinnerStatistics,
   getSkipboGameStatisticsGeneral,
-  getSkipboGameWinners,
   getTopWinners,
 } from '../../util/apiclient';
 import { SkipboGame } from '../../interfaces/skipboGame';
@@ -16,7 +15,6 @@ import styles from './SkipboOverviewPage.module.css';
 import PieChartStatisticCard from '../../components/PieChartStatisticCard';
 import ColumnStatisticCard from '../../components/ColumnStatisticCard';
 
-// TODO Handle the case, when there are no data
 function SkipboOverviewPage() {
   const [isLoading, setLoading] = useState(true);
   const [skipboGames, setSkipboGames] = useState([] as SkipboGame[]);
