98eee54e25f8e67175085f71b04f97c6d1d25c21
Guido Lange <g.lange@thalia.de>
2020-06-27 12:37:48 +0200

    Add GeneralStatisticCard

---
+++ src/public/react/src/components/GeneralStatisticCard/index.tsx
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import ReloadableCard, { Props as ReloadableStatisticCardProps } from '../ReloadableCard';
+import { Table } from 'react-bootstrap';
+import { GeneralStatistic } from '../../interfaces/skipboGame';
+
+//import styles from './GeneralStatisticCard.module.css';
+
+export type Props = Omit<ReloadableStatisticCardProps, 'children'>;
+
+const GeneralStatisticCard: React.FC<Props> = ({ title, fetchData }: Props) => {
+  const [data, setData] = useState({});
+  const loadData = (): Promise<void> => {
+    return fetchData().then((data: GeneralStatistic | any[]) => setData(data));
+  };
+
+  return (
+    <ReloadableCard title={title} fetchData={loadData}>
+      {!data ? null : Array.isArray(data) ? null : (
+        <Table responsive striped>
+          <tbody>
+            {Object.keys(data).map((key) => {
+              const currData = (data as GeneralStatistic)[key];
+              return (
+                <tr key={key}>
+                  <td>{currData.label}</td>
+                  <td>{currData.value}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </Table>
+      )}
+    </ReloadableCard>
+  );
+};
+
+export default GeneralStatisticCard;
