c00b6be24e094b6aef35bf2d0f30d3272d0bec89
Guido Lange <g.lange@thalia.de>
2020-06-27 12:38:13 +0200

    Use CSS as module in App

--- src/public/react/src/App.tsx
+++ src/public/react/src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './App.css';
+import './App.module.css';
 import routes from './util/routes';
 import { Route, Switch } from 'react-router-dom';
 import HomePage from './pages/HomePage';
@@ -7,13 +7,15 @@ import SkipboOverviewPage from './pages/SkipboOverviewPage';
 import SkipboAddGamePage from './pages/SkipboAddGamePage';
 import Header from './components/Header';
 
+import styles from './App.module.css';
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className={styles.App}>
+      <header className={styles.Header}>
         <Header />
       </header>
-      <main className="App-main">
+      <main className={styles.Main}>
         <Switch>
           <Route path={routes.skipboAddGame}>
             <SkipboAddGamePage />
