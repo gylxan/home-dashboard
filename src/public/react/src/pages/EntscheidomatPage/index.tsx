4040fc16259129ffa8166310b8b4c30c77cf652a
Guido Lange <g.lange@thalia.de>
2020-07-19 13:26:26 +0200

    Add document title and todo to EntscheidomatPage

--- src/public/react/src/pages/EntscheidomatPage/index.tsx
+++ src/public/react/src/pages/EntscheidomatPage/index.tsx
@@ -1,13 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { getPageTitle } from '../../util/routes';
 
 //import styles from './EntscheidomatPage.module.css';
 
-
-
 export interface Props {}
 
-const EntscheidomatPage: React.FC<Props> = () => (
-  <div className="EntscheidomatPage">Hier kommt der Entscheidomat hin!</div>
-);
+// TODO Implement me!
+const EntscheidomatPage: React.FC<Props> = () => {
+  useEffect(() => {
+    document.title = getPageTitle('Entscheidomat');
+  }, []);
+  return <div className="EntscheidomatPage">Hier kommt der Entscheidomat hin!</div>;
+};
 
 export default EntscheidomatPage;
