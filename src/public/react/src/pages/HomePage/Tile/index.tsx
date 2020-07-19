1add42e4541e1fa3a421f566165069a7f7e46e9d
Guido Lange <g.lange@thalia.de>
2020-07-19 21:00:37 +0200

    Add icons to tiles

--- src/public/react/src/pages/HomePage/Tile/index.tsx
+++ src/public/react/src/pages/HomePage/Tile/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import styles from './Tile.module.css';
 import { Link } from 'react-router-dom';
 import routes from 'util/routes';
@@ -6,14 +6,18 @@ import routes from 'util/routes';
 export interface Props {
   name: string;
   label: string;
+  icon?: string;
   description?: string;
   backgroundColor?: string;
 }
 
-const Tile: React.FC<Props> = ({ name, label, backgroundColor }: Props) => (
-  <Link className={styles.Tile} to={routes[name]} style={{ backgroundColor }}>
-    <h5>{label}</h5>
-  </Link>
-);
+const Tile: React.FC<Props> = ({ name, label, backgroundColor, icon }: Props) => {
+  return (
+    <Link className={styles.Tile} to={routes[name]} style={{ backgroundColor }}>
+      {!!icon && <img className={styles.Icon} src={icon} alt="icon" />}
+      <h5>{label}</h5>
+    </Link>
+  );
+};
 
 export default Tile;
--- src/public/react/src/pages/HomePage/index.tsx
+++ src/public/react/src/pages/HomePage/index.tsx
@@ -5,8 +5,15 @@ import { Board } from '../../interfaces/board';
 import { getPageTitle } from 'util/routes';
 import Tile from './Tile';
 import { DEFAULT_COLORS } from '../../util/colors';
+import cardGameIcon from '../../assets/icons/card-game.png';
+import dicesIcon from '../../assets/icons/dices.png';
 
 import styles from './HomePage.module.css';
+
+const BOARD_ICONS: { [gameName: string]: string } = {
+  skipbo: cardGameIcon,
+  entscheidomat: dicesIcon,
+};
 
 function HomePage() {
   const [isLoading, setLoading] = useState(true);
@@ -34,6 +41,7 @@ function HomePage() {
                 label={board.label}
                 description={board.description}
                 backgroundColor={DEFAULT_COLORS[index]}
+                icon={BOARD_ICONS[board.name] || undefined}
               />
             </Col>
           ))}
