c0f445bb7a1bda9889422e6aedb2a07f0e9d9512
Guido Lange <g.lange@thalia.de>
2021-02-07 12:02:35 +0100

    Close header collapse when item is selected

--- src/public/react/src/components/Header/index.tsx
+++ src/public/react/src/components/Header/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Nav, Navbar } from 'react-bootstrap';
 import routes, { linkTo } from 'util/routes';
 import { Link, useLocation } from 'react-router-dom';
@@ -10,11 +10,23 @@ import LinkButton from '../LinkButton';
 export interface Props {}
 
 const Header: React.FC<Props> = () => {
+  const [isExpanded, setExpanded] = useState(false);
   const location = useLocation();
   const isSkipboPage = location.pathname === routes.skipbo;
   const isSkipboTablePage = location.pathname === routes.skipboTable;
+
+  const collapse = (): void => setExpanded(false);
+  const toggle = (): void => setExpanded(!isExpanded);
+
   return (
-    <Navbar bg="light" expand="lg" className={styles.Header}>
+    <Navbar
+      bg="light"
+      expand="lg"
+      className={styles.Header}
+      expanded={isExpanded}
+      onSelect={collapse}
+      onToggle={toggle}
+    >
       <Navbar.Brand>
         <Link to={linkTo.home()} className={styles.HomeLink}>
           <img className={styles.Logo} src={logo} alt="logo" />
@@ -27,18 +39,18 @@ const Header: React.FC<Props> = () => {
           <Navbar.Collapse id="skipbo-navbar-nav">
             <Nav>
               {isSkipboTablePage && (
-                <Link className="nav-link" to={linkTo.skipbo()}>
+                <Link className="nav-link" to={linkTo.skipbo()} onClick={collapse}>
                   Grafikübersicht
                 </Link>
               )}
               {isSkipboPage && (
-                <Link className="nav-link" to={linkTo.skipboTable()}>
+                <Link className="nav-link" to={linkTo.skipboTable()} onClick={collapse}>
                   Tabellenübersicht
                 </Link>
               )}
             </Nav>
             <Nav className="ml-auto">
-              <LinkButton to={linkTo.skipboAddGame()} variant="outline-primary" type="button">
+              <LinkButton to={linkTo.skipboAddGame()} variant="outline-primary" type="button" onClick={collapse}>
                 Spiel hinzufügen
               </LinkButton>
             </Nav>
