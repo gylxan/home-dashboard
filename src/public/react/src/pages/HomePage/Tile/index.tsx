fe430b3fcfe022df3d7366c9ffab50a776b28a5c
Guido Lange <g.lange@thalia.de>
2021-03-14 13:03:04 +0100

    Use Typography in HomePage Tile

--- src/public/react/src/pages/HomePage/Tile/index.tsx
+++ src/public/react/src/pages/HomePage/Tile/index.tsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import routes from 'util/routes';
 import Icon from '../../../components/Icon';
 import { IconProp } from '@fortawesome/fontawesome-svg-core';
+import Typography from "../../../components/Typography";
 
 import styles from './Tile.module.css';
 
@@ -20,7 +21,7 @@ const Tile: React.FC<Props> = ({ name, label, backgroundColor, imagePath, icon }
     <Link className={styles.Tile} to={routes[name]} style={{ backgroundColor }}>
       {!!imagePath && <img className={styles.Icon} src={imagePath} alt="icon" />}
       {!!icon && <Icon className={styles.Icon} icon={icon} color="black" size="4x" />}
-      <h5>{label}</h5>
+      <Typography variant="h6">{label}</Typography>
     </Link>
   );
 };
