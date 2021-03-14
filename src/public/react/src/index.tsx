e3f8175597d3524d72b9bf3646e5281c14b42d4d
Guido Lange <g.lange@thalia.de>
2021-03-14 13:02:24 +0100

    Remove fontsource-roboto

--- src/public/react/src/index.tsx
+++ src/public/react/src/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
-import 'fontsource-roboto';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from 'react-router-dom';
