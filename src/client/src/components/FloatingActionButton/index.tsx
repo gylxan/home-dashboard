14d42cb8b112efdbf3ada6bf314008be6e2376b7
Guido Lange <g.lange@thalia.de>
2021-08-31 19:56:08 +0200

    Use higher z-index in FloatingActionButton

--- src/client/src/components/FloatingActionButton/index.tsx
+++ src/client/src/components/FloatingActionButton/index.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import { Fab as MuiFab, FabProps } from '@material-ui/core';
+import classNames from 'classnames';
+
+import styles from './FloatingActionButton.module.css';
 
 export type Props = FabProps;
 
-const FloatingActionButton: React.FC<Props> = ({ color = 'secondary', ...props }) => (
-  <MuiFab color={color} {...props} />
+const FloatingActionButton: React.FC<Props> = ({ color = 'secondary', className, ...props }) => (
+  <MuiFab color={color} className={classNames(styles.FloatingActionButton, className)} {...props} />
 );
 
 export default FloatingActionButton;
